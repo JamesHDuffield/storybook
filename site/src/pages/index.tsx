f2318a4e6eac2c581426d22c7f6c1308dd75874f
James Duffield <james.h.duffield@gmail.com>
2023-03-24 14:28:40 +1100

    feat(site): :lipstick: add colours and formatting to story pages

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -1,27 +1,34 @@
 import { Story } from '@/models/story'
 import { stringify } from 'querystring';
+import styles from '../styles/index.module.css'
 
 interface IProps {
-  story?: Story,
+  story: Story | null,
 }
 
 export default function Home({ story }: IProps) {
+
   if (!story) {
-    return <div>No story available</div>
+    return <div className={styles.container}><h3>Sorry, no story available. Check back later.</h3></div>
   }
+
+  const pages = story.content.split('\n');
+
   return (
     <>
-    <div>
-      <h1>{story.title}</h1>
-      <p>{story.content}</p>
+    <div className={styles.container}>
+      <h1 className={styles.title}>{story.title}</h1>
+      {pages.map((page, i) => (
+        <p key={i}>{page}</p>
+      ))}
     </div>
     </>
   )
 }
 
 export async function getServerSideProps(): Promise<{ props: IProps }> {
-  const query = stringify({ sort: '-createdAt', limit: 1, where: '[status][equals]=approved' })
+  const query = stringify({ sort: '-createdAt', page: 1, limit: 1, 'where[status][equals]': 'approved' })
   const res = await fetch(`${process.env.API_URL}/stories?${query}`);
   const data: { docs: Story[] } = await res.json();
-  return { props: { story: data.docs[0] } }
+  return { props: { story: data.docs[0] ?? null } }
 }
